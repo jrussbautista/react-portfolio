29061b6ca65b4cfb656ab425755f3a4a90463d22
jrussumbrella <jrussclay@gmail.com>
2019-07-03 14:00:28 +0800

    Add modal to projects info

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -7,20 +7,60 @@ const portfolioData = {
 	"intro": "Let's keep building amazing stuff.",
 	"projects": [
 		{
-			"title": "Hataw Ecommerce",
-			"imageUrl": "images/portfolio/hataw.png",
-			"link": ""
+			"id": 1,
+			"title": "Hataw Ecommerce Website",
+			"imageUrl": "images/portfolio/hataw-thumbnail.png",
+			"link": "https://hataw.ph/",
+			"codeLink": "",
+			"description": "Hataw is an Ecommerce website that I was the one who do front-end stuff.",
+			"technology": ["HTML/CSS", "SASS(SCSS)", "Javascript", "Zepto Js / Jquery"],
+		},
+
+		{
+			"id": 3,
+			"title": "Portfolio using React ",
+			"imageUrl": "images/portfolio/react-portfolio-thumbnail.png",
+			"link": "",
+			"codeLink": "https://github.com/jrussumbrella/react-portfolio",
+			"description": "My personal portfolio showcasing my projects that was written in React.",
+			"technology": ["HTML",  "SASS/SCSS", "React"]				
 		},
 		{
-			"title": "Admin Dashboard",
-			"imageUrl": "images/portfolio/hataw.png",
-			"link": ""
+			"id": 4,			
+			"title": "Paper Todo App using React",
+			"imageUrl": "images/portfolio/react-todo-thumbnail.png",
+			"link": "",
+			"codeLink": "https://github.com/jrussumbrella/react-paper-todo",
+			"description": "Simple to do app that uses local storage to save to do list.",
+			"technology": ["HTML", "Paper CSS", "React"]			
 		},
 		{
-			"title": "Smash It",
-			"imageUrl": "images/portfolio/hataw.png",
-			"link": ""
-		}				
+			"id": 5,
+			"title": "MHI 2K18 Web based Tabulation System Demo",
+			"imageUrl": "images/portfolio/mhi-thumbnail.jpg",
+			"link": "",
+			"codeLink": "https://github.com/jrussumbrella/mhi-web-based-tabulation",
+			"description": "It is a network based system that I developed to easily tabulate the scored of certain candidates per judge.",
+			"technology": ["HTML/CSS", "Bootstrap", "Javascript/Jquery", "Mysql", "PHP" ]				
+		},
+		{
+			"id": 6,			
+			"title": "Online Registration using Laravel",
+			"imageUrl": "images/portfolio/attendance-thumbnail.png",
+			"link": "http://doitpsualaminos.herokuapp.com/",
+			"codeLink": "",
+			"description": "Online Registration for certain activities in our school.",
+			"technology": ["HTML/CSS", "Bootstrap", "Javascript/Jquery", "Laravel"]				
+		},
+		{
+			"id": 7,
+			"title": "Java Made Easy Android App",
+			"imageUrl": "images/portfolio/java-made-app.jpg",
+			"link": "",
+			"codeLink": "https://github.com/jrussumbrella/java-made-easy",
+			"description": "This projects was my thesis that aimed to help those students especially IT students or those people who wants to learn java programming language.",
+			"technology": ["XML","Java"]				
+		},								
 		],
 	"skills": [
 		{
